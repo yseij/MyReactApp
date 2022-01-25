d69ad6348537d49a1e08b005d6a8444da89a0b6e
yseij <r0790156@student.thomasmore.be>
2022-01-25 17:26:59 +0100

    add css en een component aanpassen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-
-
 function App() {
   return (
-    <div>hello</div>
+    <div>
+      <h1>My todos</h1>
+      <div className="card">
+        <h2>TITLE</h2>
+        <div>
+          <button>Delete</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
