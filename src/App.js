31883aeb4bbf5907a5bff5efbedcdc8bb503effd
yseij <r0790156@student.thomasmore.be>
2022-01-25 17:59:30 +0100

    component aangemaakt en hergebruikt

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
+import Todo from "./components/Todo";
+
 function App() {
   return (
     <div>
       <h1>My todos</h1>
-      <div className="card">
-        <h2>TITLE</h2>
-        <div>
-          <button>Delete</button>
-        </div>
-      </div>
+      <Todo />
+      <Todo />
+      <Todo />
     </div>
   );
 }
