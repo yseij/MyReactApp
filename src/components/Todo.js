d849027b81d65d0734f33bfaffcc4416955a09a5
yseij <r0790156@student.thomasmore.be>
2022-01-26 17:59:22 +0100

    event props gebruiken in components

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,22 +1,30 @@
-import {useState} from 'react'
+import { useState } from "react";
 import Backdrop from "./Backdrop";
 import Modal from "./Modal";
 
 function Todo(props) {
   const [modalIsOpen, setModalIsOpen] = useState();
 
-  function deleteHandler(){
-    setModalIsOpen(true)
+  function deleteHandler() {
+    setModalIsOpen(true);
+  }
+
+  function closeModalHandler() {
+    setModalIsOpen(false);
   }
 
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn" onClick={deleteHandler}>Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
-      { modalIsOpen && <Modal />}
-      { modalIsOpen && <Backdrop />}
+      {modalIsOpen && (
+        <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />
+      )}
+      {modalIsOpen && <Backdrop onClick={closeModalHandler} />}
     </div>
   );
 }
